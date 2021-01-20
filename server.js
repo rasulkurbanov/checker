44b1d7d072fdfab1a53a27d565941a5d8d9df02b
rasulkurbanov <rasuljohn139@gmail.com>
2021-01-20 05:23:43 +0500

    Bug fix

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8080
+const cors = require('cors')
 const fileUpload = require('express-fileupload')
 
 
@@ -9,26 +10,30 @@ app.set('view engine', 'html')
 
 
 app.use('/public', express.static('public'))
-
+app.use(cors())
+app.use(fileUpload())
 
 app.get('/', (req, res) => {
-  // res.json('Assalomu alaykum')
   res.render('index.html')
 })
 
 
-app.post('/', (req, res) => {
-  console.log(req.files)
-    if(!req.files) {
-      res.send({
-        status: false,
-        message: "No file uploaded"
-      })
-    }
-    else {
-      console.log(req.files)
-    }
+app.post('/upload', (req, res) => {
 
+  if(!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No file uploaded')
+  }
+
+  let file = req.files.uploadedFile
+  let filename = file.name
+
+  file.mv('./public/'+ filename, (err) => {
+    if(err) {
+      console.log(err)
+      return res.status(500).send(err)
+    }
+    res.send('File successfully uploaded!').end()
+  })
 })
 
 
